183ebb570a6f89ed54ad2d30b780a655d4d48fc6
Munib2221 <bmunib214@gmail.com>
2026-05-04 10:34:36 +0500

    update

--- inspect_resnet.py
+++ inspect_resnet.py
@@ -38,10 +38,7 @@ print_group(f['weights']['layer1'][first], f'layer1[{first}]')
 
 f.close()
 
-<<<<<<< HEAD
-print('Done.')
-=======
 print('Done.')
 
 
->>>>>>> 1854273c38c7f10f3bd36904a32aa2a566058d52
+
